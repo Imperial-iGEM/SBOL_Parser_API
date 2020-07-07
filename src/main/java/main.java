b4e45f65f6fd6b2f28e724eca15ae3463a5c5fad
HM00 <hiaming01@hotmail.com>
2020-07-07 22:44:32 +0100

    Described BASIC linkers in SBOL (no sequence annotations)

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 class main {
     public static void main(String[] args) throws SBOLValidationException, SBOLConversionException, IOException {
 
-        String filePath = "./examples/sbol_files/dummy.xml";
+        String filePath = "./examples/sbol_files/Medium_2016Interlab.xml";
         SBOLDocument doc = SBOLReader.read(filePath);
 
         try{
